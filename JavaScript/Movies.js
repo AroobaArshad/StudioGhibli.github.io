68bea7d8fa5bc0838f0c26a19743e06a888c38ea
Arooba Arshad <aroobaarshad04@gmail.com>
2023-12-26 20:08:51 +0400

    Update

---
+++ JavaScript/Movies.js
@@ -0,0 +1,44 @@
+let items = document.querySelectorAll(".slider .list .item");
+let back = document.getElementById("back");
+let next = document.getElementById("next");
+let slides = document.querySelectorAll(".slides .item");
+
+let countItems = items.length;
+let itemActive = 0;
+
+next.onclick = function() {
+    itemActive = itemActive + 1;
+    if(itemActive >= countItems){
+        itemActive = 0;
+    }
+    showSlider();
+}
+
+back.onclick = function() {
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItems -1;
+    }
+    showSlider();
+}
+
+let interval = setInterval(() => {
+    next.click();
+}, 5000)
+
+function showSlider(){
+    let itemActivePrev = document.querySelector(".slider .list .item.active");
+    let slidesActivePrev = document.querySelector(".slides .item.active");
+    itemActivePrev.classList.remove("active");
+    slidesActivePrev.classList.remove("active");
+
+    items[itemActive].classList.add("active");
+    slides[itemActive].classList.add("active");
+}
+
+slides.forEach((slide, index) => {
+    slide.addEventListener("click", () => {
+        itemActive = index;
+        showSlider();
+    });
+});
